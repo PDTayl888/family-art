9cea3eb23b0cc894ddbc7044339d2736683f121c
pdtayl888 <patrickdtaylor1980@gmail.com>
2020-04-11 11:48:19 -0400

    Updates

---
+++ precache-manifest.a886b7f4da8d8f52e809a69aee8ddf85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05faad870c34f668e5e02ea653d1deb",
+    "url": "/family-art/index.html"
+  },
+  {
+    "revision": "6c443a70deace9b5354e",
+    "url": "/family-art/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "82bf6dea5834b71a9fc7",
+    "url": "/family-art/static/css/main.42af080d.chunk.css"
+  },
+  {
+    "revision": "6c443a70deace9b5354e",
+    "url": "/family-art/static/js/2.9950cbc9.chunk.js"
+  },
+  {
+    "revision": "24de9f98477d647346d2b6edc6a6e070",
+    "url": "/family-art/static/js/2.9950cbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82bf6dea5834b71a9fc7",
+    "url": "/family-art/static/js/main.468d98d8.chunk.js"
+  },
+  {
+    "revision": "38b54fd209c03a9a9d88",
+    "url": "/family-art/static/js/runtime-main.acc3e1ee.js"
+  }
+]);
